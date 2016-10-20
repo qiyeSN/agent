ca3a9d6c225e8aa7f6a8e3a4ca0a06172dd6d2b3
qiyeSN <songn@uway.cn>
2016-10-20 17:08:01 +0800

    周期上报功能代码
    周期上报，需要配合其他代码。

--- cyclereport.go
+++ cyclereport.go
@@ -1,25 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"github.com/elastic/gosigar/get"
 	"github.com/robfig/cron"
-	"os"
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "protobuf/report"
+	//hs "protobuf/report/hostMessage"
+	//"os"
+)
+
+const (
+	address = "localhost:50051"
+	//defaultVersion = "1"
 )
 
 func main() {
-	spec := "0/5, *, *, *, ?, *"
+	spec := "0/30, *, *, *, ?, *"
 	c := cron.New()
-	err := c.AddFunc(spec, callYourFunc)
-	if err != nil {
-		fmt.Println("some wrong")
-		return
-	}
+	c.AddFunc(spec, report)
+	//if err != nil {
+	//	fmt.Println("some wrong")
+	//	return
+	//}
 	c.Start()
 	select {}
 }
 
 func callYourFunc() {
-	a := 0
-	fmt.Println("callYourFunc come here.")
-	fmt.Println("heheheh", 1/a)
+	//a := 0
+	log.Println("callYourFunc come here.")
+	//fmt.Println("heheheh", 1/a)
+}
+
+func report() {
+
+	hostMess := get.GetHost()
+	processMess := get.GetProcess()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	//version := defaultVersion
+	//if len(os.Args) > 1 {
+	//	version = os.Args[1]
+	//}
+	r, err := c.SendReport(context.Background(), &pb.Report{Host: &hostMess, Process: processMess})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %d", r.Mess)
 }
